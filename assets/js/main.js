c439e7d1dec0c1d05fcc7a0527231dbcad6b75d7
raktimproloy <raktimproloy@gmail.com>
2023-03-11 22:54:24 +0600

    your name question

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,13 @@
 const sendMessageInput = document.getElementById("sendMessageInput")
 const sendMessageBtn = document.getElementById("sendMessageBtn")
 
-// var url; = 'https://api.dictionaryapi.dev/api/v2/entries/en/tell';
-
-function getItems(url) {
-    fetch(url)
-      .then(response => response.json())
-      .then(data => console.log("uporar",data))
-      .catch(error => console.error('Unable to get items.', error));
-  }
-
 sendMessageBtn.addEventListener("click", () => {
     getData()
 })
 
-async function getData(){
+function getData(){
      const message = sendMessageInput.value
-    // const message = "talk me your name"
+    // const message = "can me your name"
     
     const whQues = ["what", "who", "how"]
     const getFirstWord = message.split(" ")[0].toLocaleLowerCase()
@@ -25,18 +16,31 @@ async function getData(){
         .then(response => response.json())
         .then(data => {
             var large = {}
+            var largeLength = 0
+            largeLength = data[0].meanings[0].definitions.length
             large = data[0].meanings[0]
+
+            // console.log(largeLength);
             data.forEach(element => {
+                // for(let i = 1; i < element.meanings.length; i ++){
+                //     console.log("new under",element.meanings[i]);
+                // }
                 element.meanings.forEach(underData => {
+                    console.log("under",underData);
                     if(underData.definitions.length > large.definitions.length){
                         large = underData
                     }
                 })
             });
+            
+            // console.log("Data",data);
+            // console.log("Large",large);
             if(large.partOfSpeech === "verb"){
                 console.log("This is a question. it started with verb");
+                questionHandler(message)
             }else if(whQues.includes(getFirstWord)){
                 console.log("This is a question. it started with WH question");
+                questionHandler(message)
             }else{
                 console.log("This is not a question");
             }
@@ -45,5 +49,10 @@ async function getData(){
      
 }
 
-
+function questionHandler(question){
+    if(question.includes("your name")){
+        console.log("my name is web-assistant");
+    }
+    
+}
 
